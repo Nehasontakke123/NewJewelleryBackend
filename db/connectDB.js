0d4b163af242914e2e129cd5d154090448b50e53
Neha <nehasontakke1880@gmail.com>
2026-02-20 14:35:45 +0530

    updated code

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,35 +1,18 @@
 
-// import mongoose from "mongoose";
-
-// const connectDB = async (DBURL, DBNAME) => {
-//   try {
-//     console.log("Connecting to DB:", DBURL, DBNAME); // Debugging
-//     await mongoose.connect(DBURL, {
-//       dbName: DBNAME,
-//       // useNewUrlParser: true,
-//       // useUnifiedTopology: true,
-//     });
-//     console.log("Database connected successfully!");
-//   } catch (error) {
-//     console.error("Error occurred while connecting to DB:", error.message);
-//     process.exit(1);
-//   }
-// };
-
-// export default connectDB;
-
-
 import mongoose from "mongoose";
 
-const connectDB = async (DBURL) => {
+const connectDB = async (DBURL, DBNAME) => {
   try {
-    console.log("Connecting to DB:", DBURL);
-
-    await mongoose.connect(DBURL);
-
+    console.log("Connecting to DB:", DBURL, DBNAME); // Debugging
+    await mongoose.connect(DBURL, {
+      dbName: DBNAME,
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
+    });
     console.log("Database connected successfully!");
   } catch (error) {
-    console.error("DB Error:", error.message);
+    console.error("Error occurred while connecting to DB:", error.message);
+    process.exit(1);
   }
 };
 
